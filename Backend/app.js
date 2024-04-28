529e91bd1782bfcc8f68cf2f418d23eb805e1085
Kawtharmashkour <kawtharmashkour@yahoo.com>
2024-04-28 14:06:54 -0400

    Create web server using express

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,18 @@
-console.log("Helllo");
+//npm install express
+const express = require('express');
+const app = express();
+
+// To use environment var. from .env file
+// npm install dotenv
+require('dotenv/config');
+const api = process.env.API_URL;
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
+app.listen(3000, ()=> {
+    //call back func. after server creation successfuly
+    console.log(api);
+    console.log('Server running on local host with port 3000')
+});
